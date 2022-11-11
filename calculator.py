987eb81bb1720c7add9d24da923496e310578fc9
sabari <r.sabarivasan10@gmail.com>
2022-11-11 21:05:49 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -14,8 +14,3 @@ def div(x,y):
     if y == 0:
         raise ValueError("Zero can't be divded by any number")
     return x/y
-
-x, y = list(map(int,(input("Enter the numbers(x,y)")).split(",")))
-op = input("Enter the operation to be performed:")
-func = {'add':add,'sub':sub,'mul':mul,'div':div}
-print(func[op](x,y))
